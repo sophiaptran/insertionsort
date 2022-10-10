4e7ca5eab54484583d666b16d6c2676577c3347e
sophia-tran <Sophia Tran>
2022-10-09 22:27:13 -0500

    insertion ordered 0

--- main.swift
+++ main.swift
@@ -1,35 +1,33 @@
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedStrings = ["paper", "tree", "apple", "book", "sun"]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+var words = [String]()
+var line = readLine()
+while line != nil && line != "" && line != " " {
+    words.append(line!)
+    line = readLine()
+}
 
-
-// Add your code below:
-
-var tempArray = unsortedStrings
+var tempArray = words
 
 var passCount = 0
 var tempCount = 0
 var totalCount = 0
 
-func swap(strings: inout [String], firstIndex: Int, secondIndex: Int) {
-    let temp = strings[firstIndex]
-    strings[firstIndex] = strings[secondIndex]
-    strings[secondIndex] = temp
+func swap(words: inout [String], firstIndex: Int, secondIndex: Int) {
+    let temp = words[firstIndex]
+    words[firstIndex] = words[secondIndex]
+    words[secondIndex] = temp
 
     tempCount += 1
 }
 
-func sort (strings: inout[String]) {
-    for i in 0..<strings.count {
+func sort (words: inout[String]) {
+    for i in 0..<words.count {
         var swapIndex = i
 
         tempCount = 0
 
         while swapIndex > 0 {
-            if strings[swapIndex] < strings[swapIndex - 1] {
-                swap(strings: &strings, firstIndex: swapIndex, secondIndex: swapIndex - 1)
+            if words[swapIndex] < words[swapIndex - 1] {
+                swap(words: &words, firstIndex: swapIndex, secondIndex: swapIndex - 1)
                 
                 swapIndex -= 1
                 totalCount += 1
@@ -39,9 +37,9 @@ func sort (strings: inout[String]) {
                 break
             }
         }
-        print("Pass: \(passCount), Swaps: \(tempCount)/\(totalCount), Array: \(strings)")
+        print("Pass: \(passCount), Swaps: \(tempCount)/\(totalCount), Array: \(words)")
 
         passCount += 1
     }   
 }
-sort(strings: &tempArray)
+sort(words: &tempArray)
